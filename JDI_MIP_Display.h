7989293fde163cc4d08be6e1547a40b6f9af6259
甘草酸不酸 <35583452+jie326513988@users.noreply.github.com>
2024-05-19 00:50:10 +0800

    Add SPI DMA transfer to ESP32（给esp32添加 spi dma传输） (#7)
    * Add SPI DMA transfer 添加spi dma传输
    
    * Add SPI DMA transfer（添加spi dma传输）
    
    将pushPixelsDMA添加多一个外部函数
    
    * Add SPI DMA transfer to ESP32（给esp32添加 spi dma传输）

--- JDI_MIP_Display.h
+++ JDI_MIP_Display.h
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+
 #ifndef JDI_MIP_Display_h
 #define JDI_MIP_Display_h
 
@@ -28,49 +29,64 @@
 #include <Adafruit_GFX.h>
 #include <Display_cfg.h>
 
-#define DIFF_LINE_UPDATE
+
+#define DIFF_LINE_UPDATE   // 差异行更新
 #define HALF_WIDTH (DISPLAY_WIDTH / 2)
 
-#define COLOR_BLACK             0x00
-#define COLOR_BLUE              0x02
-#define COLOR_GREEN             0x04
-#define COLOR_CYAN              0x06
-#define COLOR_RED               0x08
-#define COLOR_MAGENTA           0x0a
-#define COLOR_YELLOW            0x0c
-#define COLOR_WHITE             0x0e
+#define COLOR_BLACK 0x00
+#define COLOR_BLUE 0x02
+#define COLOR_GREEN 0x04
+#define COLOR_CYAN 0x06
+#define COLOR_RED 0x08
+#define COLOR_MAGENTA 0x0a
+#define COLOR_YELLOW 0x0c
+#define COLOR_WHITE 0x0e
 
-#define CMD_NO_UPDATE           0x00
-#define CMD_BLINKING_BLACK      0x10
-#define CMD_BLINKING_INVERSION  0x14
-#define CMD_BLINKING_WHITE      0x18
-#define CMD_ALL_CLEAR           0x20
-#define CMD_VCOM                0x40
-#define CMD_UPDATE              0x90
+#define CMD_NO_UPDATE 0x00
+#define CMD_BLINKING_BLACK 0x10
+#define CMD_BLINKING_INVERSION 0x14
+#define CMD_BLINKING_WHITE 0x18
+#define CMD_ALL_CLEAR 0x20
+#define CMD_VCOM 0x40
+#define CMD_UPDATE 0x90
 
-class JDI_MIP_Display : public Adafruit_GFX{
-    public:
+class JDI_MIP_Display : public Adafruit_GFX
+{
+public:
         JDI_MIP_Display();
-        void begin();
+        void begin(int sck = -1, int miso = -1, int mosi = -1, int ss = -1, int fre = -1, bool dmaEN = 0);
         void refresh();
+        void refresh2();
         void displayOn();
         void displayOff();
         void clearScreen();
         void frontlightOn();
         void frontlightOff();
         void setBackgroundColor(uint16_t color);
+        void drawBufferedPixel(int16_t x, int16_t y, uint16_t color);
+        void pushPixelsDMA(uint8_t *image, uint32_t len);
+private:
+        uint8_t _scs;         // 芯片选择信号 Chip selection signal
+        uint8_t _disp;        // 显示ON/OFF开关信号 Display ON/OFF switch signal
+        uint8_t _frontlight;  // 前置灯 Front lights
+        uint16_t _background; // 背景 background
 
-    private:
-        uint8_t _scs;       //Chip Select Signal
-        uint8_t _disp;      //Display ON/OFF Switching Signal
-        uint8_t _frontlight;
-        uint16_t _background;
         char _backBuffer[(DISPLAY_WIDTH / 2) * DISPLAY_HEIGHT];
 #ifdef DIFF_LINE_UPDATE
         char _dispBuffer[(DISPLAY_WIDTH / 2) * DISPLAY_HEIGHT];
 #endif
-        void sendLineCommand(char* line_cmd, int line);
+        void sendLineCommand(char *line_cmd, int line);
         void drawPixel(int16_t x, int16_t y, uint16_t color);
         bool compareBuffersLine(int lineIndex);
+
+        // 检查DMA是否已完成-使用while（tft.dmaBusy）；等待阻塞
+        // Check if DMA has been completed - use while (tft. dmaBusy); Waiting for blocking
+        bool DMA_Enabled = false; // DMA启用状态的标志 Flag for DMA enabled status
+        uint8_t spiBusyCheck = 0; // 要检查的ESP32传输缓冲区的数量 The number of ESP32 transmission buffers to be checked
+        bool dmaBusy(void);       // 如果DMA仍在进行中，则返回true If DMA is still in progress, return true
+        void dmaWait(void);       // 等待DMA完成 Waiting for DMA completion
+        void _pushPixelsDMA(uint8_t *image, uint32_t len);
+        bool initDMA(int sck, int miso, int mosi, int ss, int fre);
+        void deInitDMA(void);
 };
 #endif
